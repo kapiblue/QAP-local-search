3a5cb7fbc85abae255d14216ba5648f8c6f03e82
kapiblue <kapiblue@outlook.com>
2024-03-06 10:46:36 +0100

    add random pair generation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
+use utils::arange;
+use utils::generate_random_int_pair;
 use utils::permute_array;
 use utils::print_array;
 
 mod utils;
 
 fn main() {
-    let mut array: [usize; 100] = core::array::from_fn(|i| i + 1);
-    println!("Ordered list:");
+    // Create a random generator to reuse
+    let mut rng = rand::thread_rng();
+    // Create an array of zeros
+    let mut array: [u32; 100] = [0; 100];
+    println!("Zero list:");
     print_array(&array);
-    permute_array(&mut array);
+    // Fill the array with ordered values
+    arange(&mut array, 0, 1);
+    println!("\nOrdered list:");
+    print_array(&array);
+    // Randomize the array
+    permute_array(&mut rng, &mut array);
     println!("\nRandom permutation:");
     print_array(&array);
+    let (x1, x2) = generate_random_int_pair(&mut rng, 8);
+    println!("\nRandom pair: {}, {}", x1, x2);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,10 @@
 use rand::Rng;
 
-pub fn permute_array(array: &mut [usize]) {
-    let mut rng = rand::thread_rng();
+pub fn permute_array<R: Rng + ?Sized>(rng: &mut R, array: &mut [u32]) {
     // Generate a random integer
     let len = (&array).len();
     let mut initial_range: usize = len;
-    let mut tmp: usize;
+    let mut tmp: u32;
     let mut exchange_index: usize;
     for _i in 0..array.len() {
         exchange_index = rng.gen_range(0..initial_range);
@@ -16,7 +15,22 @@ pub fn permute_array(array: &mut [usize]) {
     }
 }
 
-pub fn print_array(array: &[usize]) {
+pub fn generate_random_int_pair<R: Rng + ?Sized>(rng: &mut R, range: u32) -> (u32, u32) {
+    let x1: u32 = rng.gen_range(0..range);
+    let x2: u32 = (rng.gen_range(0..range - 1) + 1 + x1) % range;
+
+    return (x1, x2);
+}
+
+pub fn arange(array: &mut [u32], low: u32, step: u32) {
+    let mut current:u32 = low;
+    for i in 0..array.len() {
+        array[i] = current;
+        current += step;
+    }
+}
+
+pub fn print_array(array: &[u32]) {
     for i in 0..array.len() {
         print!("{} ", array[i]);
     }
